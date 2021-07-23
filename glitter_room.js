0af61591dd5d810245301e1b7fd63d9f32d3a239
Satana145 <76929429+Satana145@users.noreply.github.com>
2021-07-22 19:54:55 -0700

    Add files via upload

--- glitter_room.js
+++ glitter_room.js
@@ -1,13 +1,15 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 var firebaseConfig = {
-      apiKey: "AIzaSyCIAe70MAf6KgY9yEcYNk1-0I1VHWYCK_I",
-      authDomain: "social-website-d5573.firebaseapp.com",
-      projectId: "social-website-d5573",
-      storageBucket: "social-website-d5573.appspot.com",
-      messagingSenderId: "239021516069",
-      appId: "1:239021516069:web:8f9ccbe11d504f0450a6cc"
+      apiKey: "AIzaSyBdy425ctLgClTEJrMCPJnZLIwaGBOXYPQ",
+      authDomain: "kwitter2-995d7.firebaseapp.com",
+      databaseURL: "https://kwitter2-995d7-default-rtdb.firebaseio.com",
+      projectId: "kwitter2-995d7",
+      storageBucket: "kwitter2-995d7.appspot.com",
+      messagingSenderId: "819795410841",
+      appId: "1:819795410841:web:270d3a06b3ef91bb31dd08"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
